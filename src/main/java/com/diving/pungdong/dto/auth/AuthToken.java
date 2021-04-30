afe7f08c272ab761c30e40b39cbb0b627a953753
Kimyechan <vlvkcjswo7@naver.com>
2021-04-30 21:14:21 +0900

    Feat: Interlock Authorization Server When Login
    solved #39
    get jwt access token, refresh token

---
+++ src/main/java/com/diving/pungdong/dto/auth/AuthToken.java
@@ -0,0 +1,13 @@
+package com.diving.pungdong.dto.auth;
+
+import lombok.Data;
+
+@Data
+public class AuthToken {
+    private String access_token;
+    private String token_type;
+    private String refresh_token;
+    private Integer expires_in;
+    private String scope;
+    private String jti;
+}
---
+++ src/main/java/com/diving/pungdong/service/AuthService.java
@@ -0,0 +1,48 @@
+package com.diving.pungdong.service;
+
+import com.diving.pungdong.dto.auth.AuthToken;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final RestTemplate restTemplate;
+
+    @Value("${security.oauth2.client.client-id}")
+    private String clientId;
+
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
+
+    public AuthToken getAuthToken(String email, String password) {
+        String credentials = clientId + ":" + clientSecret;
+        String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+        headers.add("Authorization", "Basic " + encodedCredentials);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("client_id", "auth_id");
+        params.add("scope", "read");
+        params.add("username", email);
+        params.add("password", password);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+        ResponseEntity<AuthToken> response = restTemplate.postForEntity("http://localhost:8095/oauth/token", request, AuthToken.class);
+
+        return response.getBody();
+    }
+}
