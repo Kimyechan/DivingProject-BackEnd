99bc989b9ceb25353b714f61628946e683e504aa
Kimyechan <vlvkcjswo7@naver.com>
2021-05-26 20:16:07 +0900

    Docs: Filter Search Lecture List API
    solved #56
    api docs tag: #resource-lecture-search-filter-list

--- src/test/java/com/diving/pungdong/controller/lecture/LectureControllerTest.java
+++ src/test/java/com/diving/pungdong/controller/lecture/LectureControllerTest.java
@@ -656,6 +656,48 @@ class LectureControllerTest {
                 .param("size", String.valueOf(pageable.getPageSize()))
                 .content(objectMapper.writeValueAsString(condition)))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(
+                        document(
+                                "lecture-search-filter-list",
+                                requestHeaders(
+                                        headerWithName(HttpHeaders.CONTENT_TYPE).description("application json 타입"),
+                                        headerWithName(HttpHeaders.AUTHORIZATION).optional().description("access token 값")
+                                ),
+                                requestParameters(
+                                        parameterWithName("page").description("몇 번째 페이지"),
+                                        parameterWithName("size").description("한 페이지 당 크기")
+                                ),
+                                requestFields(
+                                        fieldWithPath("organization").description("자격증 단체 조건"),
+                                        fieldWithPath("level").description("자격증 레벨 조건"),
+                                        fieldWithPath("region").description("강의 지역 조건"),
+                                        fieldWithPath("classKind").description("강의 종류 조건"),
+                                        fieldWithPath("costCondition.max").description("강의료 최대 바용"),
+                                        fieldWithPath("costCondition.min").description("강의료 최저 바용")
+                                ),
+                                responseFields(
+                                        fieldWithPath("_embedded.lectureInfoList[].id").description("인기 강의 식별자 값"),
+                                        fieldWithPath("_embedded.lectureInfoList[].title").description("인기 강의 제목"),
+                                        fieldWithPath("_embedded.lectureInfoList[].organization").description("인기 강의 자격증 단체 이"),
+                                        fieldWithPath("_embedded.lectureInfoList[].level").description("인기 강의 자격증 레벨"),
+                                        fieldWithPath("_embedded.lectureInfoList[].region").description("인기 강의 지역명"),
+                                        fieldWithPath("_embedded.lectureInfoList[].maxNumber").description("인기 강의 최대 인원수"),
+                                        fieldWithPath("_embedded.lectureInfoList[].maxNumber").description("인기 강의 최대 인원수"),
+                                        fieldWithPath("_embedded.lectureInfoList[].lectureTime").description("인기 강의 총 강의 시간"),
+                                        fieldWithPath("_embedded.lectureInfoList[].isMarked").description("인기 강의 찜 여부"),
+                                        fieldWithPath("_embedded.lectureInfoList[].price").description("인기 강의 강의 비용"),
+                                        fieldWithPath("_embedded.lectureInfoList[].imageUrl").description("인기 강의 대표 이미지"),
+                                        fieldWithPath("_embedded.lectureInfoList[].equipmentNames[]").description("인기 강의 대여 장비 목록"),
+                                        fieldWithPath("_embedded.lectureInfoList[].starAvg").description("인기 강의 리뷰 종합 평점"),
+                                        fieldWithPath("_embedded.lectureInfoList[].reviewCount").description("인기 강의 리뷰 갯수"),
+                                        fieldWithPath("_links.self.href").description("해당 Api Url"),
+                                        fieldWithPath("page.size").description("한 페이지 당 사이즈"),
+                                        fieldWithPath("page.totalElements").description("전체 신규 강의 갯수"),
+                                        fieldWithPath("page.totalPages").description("전체 페이지 갯수"),
+                                        fieldWithPath("page.number").description("현재 페이지 번호")
+                                )
+                        )
+                );
     }
 }
