a78300a1e5ce4b866cab12a4af2f546a5b8765fe
Kimyechan <vlvkcjswo7@naver.com>
2021-07-25 15:27:25 +0900

    Refactor: Find Like Lecture Map
    solved #147
    if like lecture, put true to map

--- src/main/java/com/diving/pungdong/repo/LectureMarkJpaRepo.java
+++ src/main/java/com/diving/pungdong/repo/LectureMarkJpaRepo.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LectureMarkJpaRepo extends JpaRepository<LectureMark, Long> {
@@ -20,4 +21,6 @@ public interface LectureMarkJpaRepo extends JpaRepository<LectureMark, Long> {
 
     @Query("select lm from LectureMark lm where lm.account.id = :accountId and lm.lecture.id = :lectureId")
     Optional<LectureMark> findByAccountAndLecture(@Param("accountId") Long accountId, @Param("lectureId") Long lectureId);
+
+    List<LectureMark> findByAccount(Account account);
 }
